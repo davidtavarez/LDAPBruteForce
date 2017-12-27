0da35a7c7f46dc3113ef4689f7236523eb24e8da
David Tavarez <d.tavarez@gbh.com.do>
2017-12-27 08:57:27 -0400

    add server

--- bruter.php
+++ bruter.php
@@ -1,18 +1,24 @@
 <?php
-$arguments = getopt("u:p:");
-if(count($arguments) !== 2 ){
-    exit(1);
+const ERROR_MISSING_ARGUMENTS = 1;
+const ERROR_LDAP_NOT_FOUND = 2;
+const ERROR_FILE_NOT_FOUND = 5;
+
+$arguments = getopt("s:u:p:");
+if(count($arguments) !== 3 ){
+    exit(ERROR_MISSING_ARGUMENTS);
 }
 if (file_exists($arguments['u']) === false || file_exists($arguments['p']) === false){
-    exit(2);
+    exit(ERROR_FILE_NOT_FOUND);
 }
+
 $users_handle = fopen($arguments['u'], "r");
 while (!feof($users_handle)) {
     $user = str_replace(array('.', "\n", "\t", "\r"), '', $trim(fgets($users_handle)));        
     $passwords_handle = fopen($arguments['p'], "r");
     while (!feof($passwords_handle)) 
     {
-
+        $password = str_replace(array("\n", "\t", "\r"), '', $trim(fgets($passwords_handle)));    
     }
+    fclose($passwords_handle);
 }
 fclose($users_handle);
